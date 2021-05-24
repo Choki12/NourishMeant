8c8d817386b3264ab641c988a0a475e3d89b2779
Choki12 <tumanechoki@gmail.com>
2021-05-24 21:57:44 +0200

    Added fridge and fruit sprites. SpriteMode altered to allow rendering of sprites on top of each other.

--- Play.cs
+++ Play.cs
@@ -22,20 +22,36 @@ namespace NourishMeant
         Texture2D fridge;
         SpriteFont font;
         Texture2D mySprite;
+        Texture2D fruit;
+        
         public Play(GraphicsDevice graphicsDevice) :base(graphicsDevice)
         {
           
         }
+
+        /*Written by Boitshoko Tumane*/
         public override void Draw(SpriteBatch spriteBatch)
         {
             _graphicsDevice.Clear(Color.White);
-            spriteBatch.Begin();
+            spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
 
             // Draw sprites here
             //spriteBatch.Draw(background2, new Vector2(150, 150), Color.White);
-            spriteBatch.DrawString(font, "Choose your ingredients", new Vector2(200, 600), Color.Black,0, new Vector2(0,0), 7.0f, SpriteEffects.None, 0.0f);
+            spriteBatch.DrawString(font, "Choose your ingredients", new Vector2(200, 200), Color.Black,0, new Vector2(0,0), 4.0f, SpriteEffects.None, 0.0f);
+            //spriteBatch.Draw()
             //spriteBatch.Draw(fridge, new Vector2(300, 500), new Rectangle(10, 60, 40, 40), Color.White, 0.0f, new Vector2(0, 0), new Vector2(500, 700),  SpriteEffects.None, 0.0f);
-            spriteBatch.Draw(fridge, new Vector2(0, 60), Color.White);
+            spriteBatch.Draw(fridge, new Vector2(0, 400), Color.White);
+            for (int y  = 0; y < 400; y++)
+            {
+                for(int x = 0; x < 50; x++)
+                {
+                 
+                    spriteBatch.Draw(fruit, new Vector2(350, 850), Color.White); // 1st row clamp @ y = 850
+                    
+                }
+            }
+            
+            
 
             spriteBatch.End();
         }
@@ -50,8 +66,10 @@ namespace NourishMeant
             content.RootDirectory = "Content";
             background2 = content.Load<Texture2D>("1f");
             font = content.Load<SpriteFont>("Font");
-            fridge = content.Load<Texture2D>("openfridge1");
-            
+            fridge = content.Load<Texture2D>("openfridge3");
+            fruit = content.Load<Texture2D>("CharactersBright_Line1");
+           
+
         }
 
         public override void UnloadContent()
